beebf0b9d4ac8c7beaf4d664ce8191bc3a404071
UIT-20521719 <20521719@ms.uit.edu.vn>
2024-02-06 08:42:51 +0700

    update user

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -5,12 +5,14 @@ import { In, Repository } from 'typeorm';
 import { CreateRecipeDto } from './dto/create-recipe/create-recipe.dto';
 import { UpdateRecipeDto } from './dto/update-recipe/update-recipe.dto';
 import { RecipeEntity } from './entities/recipe.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class RecipeService {
   constructor(
     @InjectRepository(RecipeEntity)
     private readonly recipeRepository: Repository<RecipeEntity>,
+    private readonly userService: UserService,
   ) {}
 
   create(createRecipeDto: CreateRecipeDto & { user: UserDto }) {
@@ -29,8 +31,33 @@ export class RecipeService {
     return this.recipeRepository.save(recipe);
   }
 
-  findById(id: string) {
-    return this.recipeRepository.findOne({
+  async like(recipeId: string, userId: string) {
+    const recipe = await this.recipeRepository.findOneBy({ id: recipeId });
+    const user = await this.userService.findById(userId, { likes: true });
+
+    user.likes = [
+      ...user.likes.filter((like) => like.id !== recipe.id),
+      recipe,
+    ];
+
+    await this.userService.update(user);
+
+    return user.likes.length;
+  }
+
+  async unlike(recipeId: string, userId: string) {
+    const recipe = await this.recipeRepository.findOneBy({ id: recipeId });
+    const user = await this.userService.findById(userId, { likes: true });
+
+    user.likes = [...user.likes.filter((like) => like.id !== recipe.id)];
+
+    await this.userService.update(user);
+
+    return user.likes.length;
+  }
+
+  async findById(id: string) {
+    const result = await this.recipeRepository.findOne({
       where: { id },
       relations: {
         ingredients: true,
@@ -38,8 +65,14 @@ export class RecipeService {
           images: true,
         },
         user: true,
+        likedUsers: true,
       },
     });
+    return {
+      ...result,
+      likes: result.likedUsers.length,
+      likedUsers: undefined,
+    };
   }
 
   findByIds(ids: string[]) {
